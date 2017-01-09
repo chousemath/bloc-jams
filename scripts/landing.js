4547d9913f3761f53704caa1a3ad81599f7632de
Joseph Sungpil Choi <chousemath@gmail.com>
2017-01-10 01:43:52 +0900

    parts of the script triggers on scroll to top of element

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,6 @@
-var animatePoints = function() {
-  var points = document.getElementsByClassName('point');
+var points = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
 
   var revealPoint = function(point) {
     point.style.opacity = 1;
@@ -11,7 +12,18 @@ var animatePoints = function() {
   for (var i = 0; i < points.length; i++) {
     revealPoint(points[i]);
   }
-
 };
 
-animatePoints();
+window.onload = function() {
+  if (window.innerHeight > 950) {
+    animatePoints(points);
+  }
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(points);
+    }
+  });
+};
